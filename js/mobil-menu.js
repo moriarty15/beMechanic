712b025e5aa8019f3fedb2600f1b1abbe40ce7bc
Vladimir Palamov <vladimir_palamov@mail.ru>
2021-08-09 16:13:33 +0800

    меню закрывает если нажать на навигацию

--- js/mobil-menu.js
+++ js/mobil-menu.js
@@ -3,7 +3,15 @@ console.log(closeMenu);
 closeMenu.addEventListener('click', giveClassSiteNav)
 const siteNavEl = document.querySelector('.site-nav');
 console.log(siteNavEl)
-console.log("hi i`m Morty");
+
 function giveClassSiteNav() {
    return siteNavEl.classList.toggle('is-open');
 }
+
+const w1 = document.querySelector('.site-nav__list');
+console.log(w1);
+const w2 = document.querySelectorAll('.site-nav__item');
+console.log(w2);
+w1.addEventListener('click', giveClassSiteNav)
+
+console.log("hi i`m Morty");
